2e4579f2be62bb733d9e40544a97e182f8556a09
Scortike <20chas02@gmail.com>
2022-06-20 18:27:18 +0300

    Complete Task3

--- Tasks/Task3.java
+++ Tasks/Task3.java
@@ -1,9 +1,33 @@
 package Tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task3 
 {
+    public static Integer digitalRoot(Integer number)
+    {
+        Integer tempNum, tempSum = 0;
+        List<String> separatedNumber = new ArrayList<String>(Arrays.asList(number.toString().split("")));
+        for(String i: separatedNumber)
+        {
+            tempNum = Integer.parseInt(i);
+            tempSum += tempNum;
+        }
+        number = tempSum;
+        if(number < 10)
+        {
+            return number;
+        }
+        else
+        {
+            return digitalRoot(number);
+        }
+    }
+
     public static void main(String[] args) 
     {
-        
+        System.out.println(digitalRoot(493193));
     }
 }
