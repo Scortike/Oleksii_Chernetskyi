325f494fb4e5cf2ac24400bb7349bbf1089f5415
Scortike <20chas02@gmail.com>
2022-06-20 17:58:58 +0300

    Complete Task2

--- Tasks/Task2.java
+++ Tasks/Task2.java
@@ -1,7 +1,30 @@
 package Tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task2 {
-    public static void main(String[] args) {
-        
+
+    public static Character firstNonRepeatingLetter(String word)
+    {
+        Character nonRepeatingLetter = null;
+        List<String> letters = new ArrayList<String>(Arrays.asList(word.split("")));
+        for (int i = 0; i < word.length(); i++)
+        {
+            String tempLetter = letters.remove(0);
+            if(!(letters.contains(tempLetter.toLowerCase())) && !(letters.contains(tempLetter.toUpperCase())))
+            {
+                nonRepeatingLetter = tempLetter.charAt(0);
+                return nonRepeatingLetter;
+            }
+        }
+        return nonRepeatingLetter;
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println(firstNonRepeatingLetter("stress"));
+        System.out.println(firstNonRepeatingLetter("sTreSS"));
     }
 }
