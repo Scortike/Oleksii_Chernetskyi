b54bac7b82a711271baf1467a7c34f46946f09a4
Scortike <20chas02@gmail.com>
2022-06-20 17:01:04 +0300

    Complete Task1
    Complete Task1

--- Task1.java
+++ Task1.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task1
 {
-    public static void main(String[] args) {
-        
+    public static ArrayList<Integer> getIntegersFromList (List<Object> argList)
+    {
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        for(Object i: argList)
+        {
+            if(i instanceof Integer)
+            {
+                intList.add((Integer)i);
+            }
+        }
+        return intList;
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> intList = getIntegersFromList(Arrays.asList(1,2,'a','b',"as",'1',"123"));
+        System.out.println(intList);
     }
 }
