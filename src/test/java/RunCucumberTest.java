b3442828e6921bcc836d03035135065bd9bdb86e
Scortike <20chas02@gmail.com>
2022-06-21 19:58:57 +0300

    Add RunCucumberTest

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,11 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "./src/test/java/features",
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+)
+public class RunCucumberTest {
+}
